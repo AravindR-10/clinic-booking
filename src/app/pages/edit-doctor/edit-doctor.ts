6a0a26431033369cd0e986b21535e5752d0ec8ca
Aravind <aravind.r2@gds.ey.com>
2025-11-10 16:51:15 +0530

    feat: Added Edit Doctor. User dashboard lists all doctors"

--- src/app/pages/edit-doctor/edit-doctor.ts
+++ src/app/pages/edit-doctor/edit-doctor.ts
@@ -1,11 +1,78 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { doc, Firestore, updateDoc } from '@angular/fire/firestore';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { getDoc } from 'firebase/firestore';
 
 @Component({
   selector: 'app-edit-doctor',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './edit-doctor.html',
   styleUrl: './edit-doctor.css',
 })
-export class EditDoctor {
+export class EditDoctor implements OnInit {
+
+  doctorForm!: FormGroup;
+  doctorId!: string;
+
+  constructor(private fb: FormBuilder, private router: Router, private route: ActivatedRoute, private firestore: Firestore) { }
+
+  ngOnInit() {
+    this.doctorId = this.route.snapshot.paramMap.get('id')!;
+    this.doctorForm = this.fb.group({
+      name: ['', Validators.required],
+      specialization: ['', Validators.required],
+      availableSlots: this.fb.array([]),
+    });
+    this.loadDoctorData();
+  }
+
+  get availableSlots() {
+    return this.doctorForm.get('availableSlots') as FormArray;
+  }
+
+  async loadDoctorData() {
+    const doctorRef = doc(this.firestore, 'doctors', this.doctorId);
+    const doctorSnap = await getDoc(doctorRef);
+    if (doctorSnap.exists()) {
+      const data: any = doctorSnap.data();
+      this.doctorForm.patchValue({
+        name: data.name,
+        specialization: data.specialization,
+      });
+      data.availableSlots.forEach((slot: any) => {
+        this.availableSlots.push(this.fb.group({
+          date: [slot.date, Validators.required],
+          startTime: [slot.startTime, Validators.required],
+          endTime: [slot.endTime, Validators.required],
+        }));
+      });
+    }
+  }
+
+  addSlot() {
+    this.availableSlots.push(this.fb.group({
+      date: ['', Validators.required],
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required],
+    }));
+  }
+
+  removeSlot(index: number) {
+    this.availableSlots.removeAt(index);
+  }
+
+  async updateDoctor() {
+    if (this.doctorForm.valid) {
+      const doctorRef = doc(this.firestore, 'doctors', this.doctorId);
+      await updateDoc(doctorRef, this.doctorForm.value);
+      alert('Doctor updated successfully!');
+      this.router.navigate(['/admin-dashboard']);
+    }
+    else {
+      alert('Please fill all required fields.');
+    }
+  }
 
 }
--- src/app/pages/user-dashboard/user-dashboard.ts
+++ src/app/pages/user-dashboard/user-dashboard.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
 import { LogoutButton } from '../../components/logout-button/logout-button';
+import { Firestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-dashboard',
-  imports: [LogoutButton],
+  imports: [LogoutButton, CommonModule],
   templateUrl: './user-dashboard.html',
   styleUrl: './user-dashboard.css',
 })
-export class UserDashboard {
+export class UserDashboard implements OnInit {
+  doctors: any[] = [];
+
+  constructor(private firestore: Firestore, private router: Router, private zone: NgZone, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.loadDoctors();
+  }
+
+  loadDoctors() {
+    const doctorsRef = collection(this.firestore, 'doctors');
+    onSnapshot(doctorsRef, (snapshot) => {
+      this.zone.run(() => {
+        this.doctors = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        this.cdr.detectChanges();
+      });
+    });
+  }
+
+  bookAppointment(doctorId: string) {
+    this.router.navigate(['/book-appointment', doctorId]);
+  }
 
 }
